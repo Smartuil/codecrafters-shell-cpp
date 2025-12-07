a9cab7c422058dc526f20e9c511e95b17508f0b7
Smartuil <563412673@qq.com>
2025-12-07 12:43:33 +0800

    Implement type

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,19 @@ int main() {
 	  {
 		  std::cout << command.substr(5) << std::endl;
 	  }
+	  else if (command.substr(0, 5) == "type ")
+	  {
+		  std::string target = command.substr(5);
+
+		  if (target == "echo" || target == "exit" || target == "type") 
+		  {
+			  std::cout << target << " is a shell builtin" << std::endl;
+		  }
+		  else
+		  {
+			  std::cout << target << ": not found" << std::endl;
+		  }
+	  }
 	  else
 	  {
 		  std::cout << command << ": command not found" << std::endl;
